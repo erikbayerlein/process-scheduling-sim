3d51b4629ef5cac2729181849a61f814a05dd5ed
erikbayerlein <erik.bayer@icloud.com>
2025-10-04 10:59:02 -0300

    feat: addition of response

---
+++ api/src/main/java/com/api/process_scheduling/dto/ScheduleResponse.java
@@ -0,0 +1,14 @@
+package com.api.process_scheduling.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ScheduleResponse {
+    private String message;
+}
