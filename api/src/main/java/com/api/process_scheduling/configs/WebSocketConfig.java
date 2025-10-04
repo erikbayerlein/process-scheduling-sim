f50cc2dc14cd5a08395c8b01e9f765b431bbfd85
erikbayerlein <erik.bayer@icloud.com>
2025-10-04 10:58:32 -0300

    feat: addition of web socket configs

---
+++ api/src/main/java/com/api/process_scheduling/configs/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package com.api.process_scheduling.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*") // Allow all origins for CORS
+                .withSockJS(); // Endpoint for WebSocket connections with SockJS fallback
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/process-scheduler"); // Prefix for outgoing messages
+        config.setApplicationDestinationPrefixes("/app"); // Prefix for incoming messages
+    }
+}
