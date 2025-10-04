3dcac066f3136f1027c470034c79f632c1a723bc
erikbayerlein <erik.bayer@icloud.com>
2025-10-04 09:35:06 -0300

    initial api commit

---
+++ api/src/main/java/com/api/process_scheduling/ProcessSchedulingApplication.java
@@ -0,0 +1,13 @@
+package com.api.process_scheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessSchedulingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessSchedulingApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/api/process_scheduling/ProcessSchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.process_scheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessSchedulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
