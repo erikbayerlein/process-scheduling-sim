addff2ac87f413fb97baddf2b4868be02d6147e5
Gabrigas <artroxgabriel@gmail.com>
2025-10-11 14:34:57 -0300

    refactor(strategy): scheduling algorithm interface

--- api/src/main/java/com/api/process_scheduling/services/impl/strategy/schedulingAlgorithm.java
+++ api/src/main/java/com/api/process_scheduling/services/impl/strategy/schedulingAlgorithm.java
@@ -2,9 +2,25 @@ package com.api.process_scheduling.services.impl.strategy;
 
 import com.api.process_scheduling.entities.Process;
 import com.panfutov.result.Result;
-import java.util.List;
+import org.jetbrains.annotations.NotNull;
 
 public interface schedulingAlgorithm {
 
-  Result<Process> selectNextProcess(List<Process> readyQueue, Process currentProcess);
+  /**
+   * Adiciona um processo na fila de prontos.
+   * <br>
+   * fila de prontos pode ser uma lista, fila ou qualquer outra estrutura de dados, dependendo do
+   * algoritmo
+   *
+   * @param process O processo a ser adicionado na fila de prontos
+   */
+  void addProcess(@NotNull Process process);
+
+  /**
+   * O método principal que decide qual processo deve ser executado no próximo ciclo. A lógica de
+   * seleção (Round Robin, Prioridade, etc.) está aqui dentro.
+   *
+   * @return O processo selecionado para execução, ou null se não houver nenhum.
+   */
+  Result<Process> selectNextProcess();
 }
