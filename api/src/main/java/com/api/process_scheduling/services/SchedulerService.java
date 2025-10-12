e28bce285daf705b9fb75de918ad959a9b510e35
Gabrigas <artroxgabriel@gmail.com>
2025-10-12 10:17:49 -0300

    refactor(service): update interfacen with setup method

--- api/src/main/java/com/api/process_scheduling/services/SchedulerService.java
+++ api/src/main/java/com/api/process_scheduling/services/SchedulerService.java
@@ -1,10 +1,14 @@
 package com.api.process_scheduling.services;
 
 import com.api.process_scheduling.dto.SimulationConfigMessage;
+import com.panfutov.result.Result;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 
 public interface SchedulerService {
 
-  void runSimulation(SimulationConfigMessage message, SimpMessagingTemplate messagingTemplate);
+  Result<Void> setupSimulation(SimulationConfigMessage message,
+      SimpMessagingTemplate messagingTemplate);
+
+  void runSimulation();
 }
 
